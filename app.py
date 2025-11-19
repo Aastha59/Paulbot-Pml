b1c3543759f89e2a42132f4790c3d8e450b96204
Aastha59 <aasthaarya2004@gmail.com>
2025-11-19 15:17:42 +0530

    add methods

--- app.py
+++ app.py
@@ -355,7 +355,7 @@ app.add_middleware(
     # allow_origins=["*"],
     allow_origins=["http://103.171.97.71:4047", "http://localhost:5173", "http://localhost:4047", "http://127.0.0.1:4047"],
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
     allow_headers=["*"],
 )
 # ✅ Serve React build
